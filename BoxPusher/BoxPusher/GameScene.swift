9fad37c771c5c93f4a7e4b2a31d626548f3bd716
Levi Hand <lhand1986@yahoo.com>
2016-07-10 11:42:42 -0700

    Changed graphics stuff around
    Changed around the way the sprites are added onto the scene by
    implementing them in the .sks file instead of the .swift file.
    
    Commented out all of the functioning code, and added in sections for
    variables.
    
    Also added in sections based on guidelines seen from numerous different
    well-organized and structured code examples.
    
    Provided some arrays to hold goal marker nodes and movable box sprites.
    
    Added a variable to hold the information of the last touched position,
    although I may want to opt for a d-pad style movement choice utilizing
    tap-and-drag to move the character as opposed to a tap-to-move style. I
    think the tap and drag style would suit the function of this game more
    fully.

--- BoxPusher/BoxPusher/GameScene.swift
+++ BoxPusher/BoxPusher/GameScene.swift
@@ -8,7 +8,18 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    // MARK: Variables
+    
+    var goalMarks: [SKSpriteNode] = []
+    var boxes: [SKSpriteNode] = []
+    var player: SKSpriteNode?
+    
+    var lastTouch: CGPoint? = nil
+    
+    
+    // MARK: SKScene
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -17,40 +28,58 @@ class GameScene: SKScene {
         background.size.height = frame.size.height
         background.size.width = frame.size.width
         
+        physicsWorld.contactDelegate = self
+        
+//        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        
         //Set the box sprites. The scaling has been turned off for this initial demonstration
-        let box1Sprite = SKSpriteNode(imageNamed: "box1")
+//        let box1Sprite = SKSpriteNode(imageNamed: "box1")
 //        box1Sprite.xScale = 0.5
 //        box1Sprite.yScale = 0.5
-        let box2Sprite = SKSpriteNode(imageNamed: "box2")
+//        let box2Sprite = SKSpriteNode(imageNamed: "box2")
 //        box2Sprite.xScale = 0.5
 //        box2Sprite.yScale = 0.5
-        let box3Sprite = SKSpriteNode(imageNamed: "box3")
+//        let box3Sprite = SKSpriteNode(imageNamed: "box3")
 //        box3Sprite.xScale = 0.5
 //        box3Sprite.yScale = 0.5
-        
+    
         /*The hero's sprites are initialized here. We are going to use the sprite body and add the limbs on with 
          physics joints to allow movement of the joints contextually. */
-        let heroSprite = SKSpriteNode(imageNamed: "heroBody")
-        heroSprite.name = "heroBody"
-        let heroArmLegSprite = SKSpriteNode(imageNamed: "heroLimbs")
-        heroArmLegSprite.name = "heroLimbs"
+//        let heroSprite = SKSpriteNode(imageNamed: "heroBody")
+//        heroSprite.name = "heroBody"
+//        heroSprite.physicsBody = SKPhysicsBody(rectangleOfSize: heroSprite.frame.size)
+//        heroSprite.physicsBody!.dynamic = false
+//        heroSprite.anchorPoint = CGPointMake(0,0)
+//        let heroArmLegSprite = SKSpriteNode(imageNamed: "heroLimbs")
+//        heroArmLegSprite.name = "heroLimbs"
+//        heroArmLegSprite.physicsBody = SKPhysicsBody(rectangleOfSize: heroArmLegSprite.frame.size)
+//        heroArmLegSprite.physicsBody!.dynamic = false
         
-        //Setting the positions for all of the sprites for the purposes of this demonstration
-        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
-        box1Sprite.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
-        box2Sprite.position = CGPoint(x: (box1Sprite.position.x), y: (box1Sprite.position.y + 100))
-        box3Sprite.position = CGPoint(x: (box1Sprite.position.x), y: (box1Sprite.position.y - 100))
-        heroSprite.position = CGPoint(x: (box1Sprite.position.x + 100), y: (box1Sprite.position.y))
-        heroArmLegSprite.position = CGPoint(x: (box1Sprite.position.x - 100), y: (box1Sprite.position.y))
+//        var joint = SKPhysicsJointPin.jointWithBodyA(heroSprite.physicsBody, bodyB: heroArmLegSprite.physicsBody, anchor: CGPoint(x: CGRectGetMaxX(self.heroSprite.frame), y: CGRectGetMinY(self.heroArmLegSprite.frame)))
+//        let joint = SKPhysicsJointPin.jointWithBodyA(heroSprite.physicsBody!, bodyB: heroArmLegSprite.physicsBody!, anchor: CGPoint(x: 10, y: 10))
+//        
+//        //Setting the positions for all of the sprites for the purposes of this demonstration
+//        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+//        box1Sprite.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+//        box2Sprite.position = CGPoint(x: (box1Sprite.position.x), y: (box1Sprite.position.y + 100))
+//        box3Sprite.position = CGPoint(x: (box1Sprite.position.x), y: (box1Sprite.position.y - 100))
+//        heroSprite.position = CGPoint(x: (box1Sprite.position.x + 100), y: (box1Sprite.position.y))
+//        heroArmLegSprite.position = CGPoint(x: (box1Sprite.position.x - 100), y: (box1Sprite.position.y))
+//        
+//        self.addChild(background)
+//        self.addChild(box1Sprite)
+//        self.addChild(box2Sprite)
+//        self.addChild(box3Sprite)
+//        self.addChild(heroSprite)
+//        self.addChild(heroArmLegSprite)
         
-        self.addChild(background)
-        self.addChild(box1Sprite)
-        self.addChild(box2Sprite)
-        self.addChild(box3Sprite)
-        self.addChild(heroSprite)
-        self.addChild(heroArmLegSprite)
+//        let joint = SKPhysicsJointPin.jointWithBodyA(heroSprite.physicsBody!, bodyB: heroArmLegSprite.physicsBody!, anchor: CGPoint(x: CGRectGetMaxX(heroSprite.frame), y: CGRectGetMinY(heroArmLegSprite.frame)))
+        
+//        self.physicsWorld.addJoint(joint)
         
     }
+    
+    // MARK: Touch Handling
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
@@ -71,8 +100,22 @@ class GameScene: SKScene {
             }
         }
     }
+    
+    //MARK: Updates
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    //MARK: SKPhysicsContactDelegate
+    
+    //MARK: User defined functions
+    
+    
+    
+    
+    
+    
+    
+    
 }
