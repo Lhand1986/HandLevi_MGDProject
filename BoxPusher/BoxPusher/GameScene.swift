112f9bc558de4997d6fa8bacf2a02a8600681ba9
Levi Hand <lhand1986@yahoo.com>
2016-07-16 13:29:24 -0700

    Added extra files and functional code
    Added two new sound files for moving and contact
    
    Removed the hero.swift file, as it was being unused
    
    Added a d-pad visual to the game scene
    
    Changed the sprites from being dependent upon code to display to
    utilizing the GameScene.sks for sprite and game display.
    
    Added functionality to the d-pad with a customized enumeration to
    enable character movement.
    
    Removed the old sprite display code
    
    Added a function to update the display depending on where and what the
    user is touching.
    
    Added contact handling capabilities in accordance with Apple
    documentation guidance in order to properly determine which physical
    bodies are contacting one another.
    
    Added a move action function, which would take the distance, sprite
    information, and speed to add movement based on linear interpolation.

--- BoxPusher/BoxPusher/GameScene.swift
+++ BoxPusher/BoxPusher/GameScene.swift
@@ -15,8 +15,46 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var goalMarks: [SKSpriteNode] = []
     var boxes: [SKSpriteNode] = []
     var player: SKSpriteNode?
-    
     var lastTouch: CGPoint? = nil
+    var touchedNode: String!
+    let walkRepeat: Int = 10
+    
+    enum DPad: Int {
+        case U,D,L,R
+        var direction: String {
+            switch self {
+            case U:
+                return "U"
+            case D:
+                return "D"
+            case L:
+                return "L"
+            case R:
+                return "R"
+            }
+        }
+        
+        var move: (CGVector) {
+            switch self {
+            case U: return CGVectorMake(0, 80)
+            case D: return CGVectorMake(0, -80)
+            case L: return CGVectorMake(-80, 0)
+            case R: return CGVectorMake(80, 0)
+            }
+        }
+        
+        var speed: NSTimeInterval {
+            switch self {
+            case U: return NSTimeInterval(1)
+            case D: return NSTimeInterval(1)
+            case L: return NSTimeInterval(1)
+            case R: return NSTimeInterval(1)
+            }
+        }
+        
+        
+        
+    }
     
     
     // MARK: SKScene
@@ -24,95 +62,92 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         //Initialize the background sprite node and set the height and width to equal the screen
+        let enterSound = SKAction.playSoundFileNamed("EnterStage.wav", waitForCompletion: false)
         let background = SKSpriteNode(imageNamed: "tile_aqua")
         background.size.height = frame.size.height
         background.size.width = frame.size.width
-        
         physicsWorld.contactDelegate = self
-        
-//        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        
-        //Set the box sprites. The scaling has been turned off for this initial demonstration
-//        let box1Sprite = SKSpriteNode(imageNamed: "box1")
-//        box1Sprite.xScale = 0.5
-//        box1Sprite.yScale = 0.5
-//        let box2Sprite = SKSpriteNode(imageNamed: "box2")
-//        box2Sprite.xScale = 0.5
-//        box2Sprite.yScale = 0.5
-//        let box3Sprite = SKSpriteNode(imageNamed: "box3")
-//        box3Sprite.xScale = 0.5
-//        box3Sprite.yScale = 0.5
-    
-        /*The hero's sprites are initialized here. We are going to use the sprite body and add the limbs on with 
-         physics joints to allow movement of the joints contextually. */
-//        let heroSprite = SKSpriteNode(imageNamed: "heroBody")
-//        heroSprite.name = "heroBody"
-//        heroSprite.physicsBody = SKPhysicsBody(rectangleOfSize: heroSprite.frame.size)
-//        heroSprite.physicsBody!.dynamic = false
-//        heroSprite.anchorPoint = CGPointMake(0,0)
-//        let heroArmLegSprite = SKSpriteNode(imageNamed: "heroLimbs")
-//        heroArmLegSprite.name = "heroLimbs"
-//        heroArmLegSprite.physicsBody = SKPhysicsBody(rectangleOfSize: heroArmLegSprite.frame.size)
-//        heroArmLegSprite.physicsBody!.dynamic = false
-        
-//        var joint = SKPhysicsJointPin.jointWithBodyA(heroSprite.physicsBody, bodyB: heroArmLegSprite.physicsBody, anchor: CGPoint(x: CGRectGetMaxX(self.heroSprite.frame), y: CGRectGetMinY(self.heroArmLegSprite.frame)))
-//        let joint = SKPhysicsJointPin.jointWithBodyA(heroSprite.physicsBody!, bodyB: heroArmLegSprite.physicsBody!, anchor: CGPoint(x: 10, y: 10))
-//        
-//        //Setting the positions for all of the sprites for the purposes of this demonstration
-//        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
-//        box1Sprite.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
-//        box2Sprite.position = CGPoint(x: (box1Sprite.position.x), y: (box1Sprite.position.y + 100))
-//        box3Sprite.position = CGPoint(x: (box1Sprite.position.x), y: (box1Sprite.position.y - 100))
-//        heroSprite.position = CGPoint(x: (box1Sprite.position.x + 100), y: (box1Sprite.position.y))
-//        heroArmLegSprite.position = CGPoint(x: (box1Sprite.position.x - 100), y: (box1Sprite.position.y))
-//        
-//        self.addChild(background)
-//        self.addChild(box1Sprite)
-//        self.addChild(box2Sprite)
-//        self.addChild(box3Sprite)
-//        self.addChild(heroSprite)
-//        self.addChild(heroArmLegSprite)
-        
-//        let joint = SKPhysicsJointPin.jointWithBodyA(heroSprite.physicsBody!, bodyB: heroArmLegSprite.physicsBody!, anchor: CGPoint(x: CGRectGetMaxX(heroSprite.frame), y: CGRectGetMinY(heroArmLegSprite.frame)))
-        
-//        self.physicsWorld.addJoint(joint)
-        
+        runAction(enterSound)
     }
     
-    // MARK: Touch Handling
     
+    // MARK: Touch Handling
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
+        /* Called when a touch begins */
+        updateTouches(touches)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        updateTouches(touches)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    }
+    
+    func updateTouches(touches: Set<UITouch>) {
         for touch in touches {
-            //Setting the ability to denote what object is being touched
+            lastTouch = touch.locationInNode(self)
             let location = touch.locationInNode(self)
             let node = self.nodeAtPoint(location)
-            let enterAction = SKAction.playSoundFileNamed("EnterStage.wav", waitForCompletion: true)
-            let exitAction = SKAction.playSoundFileNamed("ExitStage.wav", waitForCompletion: true)
-            if let name = node.name {
-                if name == "heroBody" {
-                    runAction(enterAction)
-                }
-                if name == "heroLimbs" {
-                    runAction(exitAction)
+            if let nodeTouch = node.name {
+                touchedNode = nodeTouch
+                let heroNode = childNodeWithName("heroBody") as! SKSpriteNode
+                if nodeTouch == "L" {
+                    moveAction(DPad.L.move, sprite: heroNode, speed: DPad.L.speed)
+                } else if nodeTouch == "R" {
+                    moveAction(DPad.R.move, sprite: heroNode, speed: DPad.L.speed)
+                } else if nodeTouch == "U" {
+                    moveAction(DPad.U.move, sprite: heroNode, speed: DPad.L.speed)
+                } else if nodeTouch == "D" {
+                    moveAction(DPad.D.move, sprite: heroNode, speed: DPad.L.speed)
                 }
             }
         }
     }
     
-    //MARK: Updates
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    //MARK: Contact handling
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        let boxSound = SKAction.playSoundFileNamed("BoxTouch.wav", waitForCompletion: true)
+        
+        guard let firstBody = contact.bodyA.node?.name else {
+            return print("FirstBodyFault")
+        }
+        guard let secondBody = contact.bodyB.node?.name else {
+            return print("SecondBodyFault")
+        }
+        if firstBody == "heroBody" && secondBody == "moveBox" {
+            runAction(boxSound)
+        }
     }
-    
-    //MARK: SKPhysicsContactDelegate
-    
     //MARK: User defined functions
     
+    /* Create a function that takes distance, which object is moving, and how fast it's moving
+     and translates that into an SKAction taken by the sprite */
+    func moveAction(distance: CGVector, sprite: SKSpriteNode, speed: NSTimeInterval) {
+        let move = SKAction.moveBy(distance, duration: speed)
+        let walkSound = SKAction.playSoundFileNamed("Walk.wav", waitForCompletion: true)
+        sprite.runAction(move)
+        for _ in 0..<walkRepeat {
+            runAction(walkSound)
+        }
+    }
+    
+    func userBoxCollision(hero: SKNode, box: SKNode) {
+        let boxSound = SKAction.playSoundFileNamed("BoxTouch.wav", waitForCompletion: true)
+        if box.name == "moveBox" {
+            runAction(boxSound)
+        }
+    }
     
     
+    /* Create an SKAction that moves the character according to the case. 
+     Example:
+     let moveAction = SKAction.moveTo(touchPosition, duration: 0.5) 
+     
+     Then, we need to tell the character to move there
+     
+     sprite!.runAction(moveAction)*/
     
     
     
